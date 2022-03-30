f689d800e2873db3b25cc311285f7d67e8ede75c
Brandon Rose <brandonrose805@gmail.com>
2022-03-29 17:39:51 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav
+           style={{
+             borderBottom: "solid 1px",
+             paddingBottom: "1rem",
+           }}
+        >
+          <Link to="/login" style={{paddingRight: "1rem"}}>Login</Link>
+          <Link to="/editor">Editor</Link>
+        </nav>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
